ffb0ab97fcc2df8529d46f499674f765622dc511
froggleston <robertdavey@carpentries.org>
2025-02-24 12:32:55 +0000

    Move all deps management to deps from init_env

--- scripts/deps.R
+++ scripts/deps.R
@@ -1,5 +1,20 @@
 library(remotes)
 
+# Set the default HTTP user agent to get pre-built binary packages
+RV <- getRversion()
+OS <- paste(RV, R.version["platform"], R.version["arch"], R.version["os"])
+codename <- sub("Codename.\t", "", system2("lsb_release", "-c", stdout = TRUE))
+options(HTTPUserAgent = sprintf("R/%s R (%s)", RV, OS))
+
+# register the repositories for The Carpentries and CRAN
+options(repos = c(
+  carpentries = "https://carpentries.r-universe.dev/",
+  CRAN = paste0("https://packagemanager.posit.co/all/__linux__/", codename, "/latest")
+))
+
+# Install the template packages to your R library
+# install.packages(c("sandpaper", "varnish", "pegboard"))
+
 sand_deps <- remotes::package_deps("sandpaper")
 varn_deps <- remotes::package_deps("varnish")
 sess_deps <- remotes::package_deps("sessioninfo")
@@ -8,6 +23,5 @@ pkgs      <- rbind(sand_deps, varn_deps, sess_deps, with_deps)
 print(pkgs)
 update(pkgs, upgrade = "always")
 
-remotes::install_version(package = "pegboard", version = Sys.getenv("PEGBOARD_VER"))
-remotes::install_version(package = "varnish", version = Sys.getenv("VARNISH_VER"))
-remotes::install_version(package = "sandpaper", version = Sys.getenv("SANDPAPER_VER"))
+# Install the template packages to your R library
+install.packages(c("sandpaper", "varnish", "pegboard"))
