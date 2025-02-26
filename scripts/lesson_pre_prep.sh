4dc27b87473eb80e9ed2299e49d38d47391a4066
froggleston <robertdavey@carpentries.org>
2025-02-26 14:54:31 +0000

    More permissions fun

--- scripts/lesson_pre_prep.sh
+++ scripts/lesson_pre_prep.sh
@@ -3,6 +3,9 @@
 # Mark Repository as Safe
 git config --global --add safe.directory /home/rstudio/lesson
 
+echo "Running as $UID $GID"
+ls -lAh /home/rstudio/lesson
+
 cd /home/rstudio/lesson
 
 # run setup
